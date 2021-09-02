2fc1a319b07fd474f74135ec9d58b05468cd404f
igamezzhen <88395668+topPythondev@users.noreply.github.com>
2021-09-03 02:41:45 +0530

    Update start.js

--- start.js
+++ start.js
@@ -213,9 +213,16 @@ var globs = {};
 globs.IPLISTEN = '127.0.0.1';
 globs.PORT_LISTEN_DIST = 8080;
 var express = require('express');
+const path = require("path");
+
 var app = express();
 app.use('/', express.static('bundleFiles'));
 app.use(express.static(__dirname));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '_dist/index.html'));
+});
+
 app.listen(globs.PORT_LISTEN_DIST, function () {
     console.log('Now opening your browser to http://localhost:8080/msgetstarted.html'.yellow);
 });
